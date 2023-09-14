9994558675b9afe181dba3e6c2495ee8f4f3b6a8
Ezerom77 <fullstack@ezerom.com.ar>
2023-09-13 23:54:32 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Ezerom77.github.io',
+  site: 'https://ezerom77.github.io',
   base: '/astro-landing-test',
 });
